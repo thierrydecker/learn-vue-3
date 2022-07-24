e935243f15c3c653a619216a2809b02a15291b6a
mail@thierry-decker.com <Themis@0710>
2022-07-24 08:29:46 +0200

    Binding text

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import {createApp} from 'vue'
+
+/*
+* Select the App component needed
+* */
+
+/* Default application*/
+// import App from './App.vue'
+
+/* Bing text application */
+import App from './BindingText.vue'
 
 createApp(App).mount('#app')
