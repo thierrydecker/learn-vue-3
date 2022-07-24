8286954c1944de349ae2be1431109a5e9f8debdf
mail@thierry-decker.com <Themis@0710>
2022-07-24 10:15:36 +0200

    BalUI buttons

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  runtimeCompiler: true,
+  // NOTE: set alias via `configureWebpack` or `chainWebpack`
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'balm-ui-plus': 'balm-ui/dist/balm-ui-plus.js',
+        'balm-ui-css': 'balm-ui/dist/balm-ui.css'
+      }
+    }
+  }
+  //chainWebpack: (config) => {
+  //  config.resolve.alias
+  //    .set('balm-ui-plus', 'balm-ui/dist/balm-ui-plus.js')
+  //    .set('balm-ui-css', 'balm-ui/dist/balm-ui.css');
+  //}
 })
